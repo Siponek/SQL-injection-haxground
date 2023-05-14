16792eec87a89a7ee81fa7dba60d26af8078126a
Siponek <szinkowicz@gmail.com>
2023-05-14 20:05:27 +0200

    Added css, sanitized login version

--- pages/php/sanitized_login.php
+++ pages/php/sanitized_login.php
@@ -3,16 +3,19 @@
 require_once 'libs/redirectFunctions.php';
 
 
-$payload = $_GET['payload'];
-echo $payload."<br>";
+echo $payload . "<br>";
 $conn = new mysqli('db', 'root', '123', 'fstt23');
+$payload = mysqli_real_escape_string(
+    $conn,
+    $_GET['payload']
+);
 $result = $conn->query("SELECT * FROM users WHERE username = '$payload'");
 
 
 while ($row = $result->fetch_assoc()) {
     $first_name = $row['first_name'];
     $last_name = $row['last_name'];
-    echo implode(" ", $row)."<br>";
+    echo implode(" ", $row) . "<br>";
 }
 if ($conn->errno) {
     redirectToLogin("Login failed!");
@@ -28,7 +31,8 @@ if ($conn->errno) {
 
 
 // Arthur' AND ExtractValue(0, CONCAT( 0x5c, User() ) ) -- -' 
+
+// SELECT * FROM products WHERE name LIKE '%%'
+
+
 $conn->close();
-
-
-?>
